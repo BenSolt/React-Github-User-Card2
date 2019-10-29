4c00c150aa63ec833ac038e496be5ff2cbbc54ac
benjamin Soltgit config user.email bensolt@gmail.com <bensolt@gmail.com>
2019-10-29 14:10:41 -0400

    starting App setup

---
+++ my-app/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import axios from 'axios';
+import ReactDOM from "react-dom";
+
+class App extends React.Component {
+
+
+  
+
+render() {
+  return (
+    <div className="App">
+      <h1>Github-User Card</h1>  
+
+
+
+
+
+    </div>
+  );
+ }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+export default App;
