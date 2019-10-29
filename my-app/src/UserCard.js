d9ca1ff09e48a114c1b5ea1242512b0f6031ceea
benjamin Soltgit config user.email bensolt@gmail.com <bensolt@gmail.com>
2019-10-29 15:29:47 -0400

    added styling

--- my-app/src/UserCard.js
+++ my-app/src/UserCard.js
@@ -11,21 +11,21 @@ const UserCard = (props) => {
             <div className='card1'>
                 <h1>{props.userC.name}</h1>
                 <img className='img1' src={props.userC.avatar_url} alt='user Image'/>
-                <h4>{props.userC.bio}</h4>
+                <h5>{props.userC.bio}</h5>
 
             </div>
 
 
-            <div className='followers'>
-                
-                <h1>Followers</h1>
-
+            <div className='cardholder2'>
+                <h2>Followers</h2>
+                <div className='followers'>
                 {props.followersC.map(follower =>
                     <div className='card2' key={follower.id}>
-                        <h1>{follower.login}</h1>
+                        <h3>{follower.login}</h3>
                         <img className='img2' src={follower.avatar_url} alt='user Image'/>
                     </div> 
                      )}
+                </div>
             </div>
         </div>
 
